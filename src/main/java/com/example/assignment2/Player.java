13d1cc61ea2bfd3319caa0d3c164b3da12b47921
Spencer Cloughley <spencercloughley@gmail.com>
2023-03-28 20:45:40 -0400

    added notes to attributes for clarification

--- src/main/java/com/example/assignment2/Player.java
+++ src/main/java/com/example/assignment2/Player.java
@@ -3,13 +3,30 @@ package com.example.assignment2;
 import java.util.ArrayList;
 
 public class Player {
+    //players first name
     private String firstName;
+
+    //players last name
     private String lastName;
+
+    //username the player goes by in game
     private String username;
+
+    //what role the player plays (Top,Jungle, Mid,ADC, Support)
     private String position;
+
+    //amount of kills the player has gotten this season
     private int kills;
+
+    //amount of deaths the player has gotten this season
     private int deaths;
+
+    //amount of assists the player has gotten this season
     private int assists;
+
+    //(kills+assists)/deaths
     private double kda;
+
+    //Characters they play in the game
     private ArrayList<String> champions;
 }
--- src/main/java/com/example/assignment2/Team.java
+++ src/main/java/com/example/assignment2/Team.java
@@ -3,8 +3,12 @@ package com.example.assignment2;
 import java.util.ArrayList;
 
 public class Team {
+    //team name
     private String name;
+    //acronym used for the team(3 characters long)
     private String acronym;
-    private ArrayList<Player> Players;
+    //list of players who are on the team(5 per team)
+    private ArrayList<Player> players;
+    //file to access the image of the team logo
     private String fileName;
 }
