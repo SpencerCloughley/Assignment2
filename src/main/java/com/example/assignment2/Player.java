ab12a439873516e545e58bee8bb017ab3ec9b7e8
Spencer Cloughley <spencercloughley@gmail.com>
2023-03-28 20:36:03 -0400

    Initial commit

---
+++ src/main/java/com/example/assignment2/Player.java
@@ -0,0 +1,15 @@
+package com.example.assignment2;
+
+import java.util.ArrayList;
+
+public class Player {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String position;
+    private int kills;
+    private int deaths;
+    private int assists;
+    private double kda;
+    private ArrayList<String> champions;
+}
---
+++ src/main/java/com/example/assignment2/Team.java
@@ -0,0 +1,10 @@
+package com.example.assignment2;
+
+import java.util.ArrayList;
+
+public class Team {
+    private String name;
+    private String acronym;
+    private ArrayList<Player> Players;
+    private String fileName;
+}
