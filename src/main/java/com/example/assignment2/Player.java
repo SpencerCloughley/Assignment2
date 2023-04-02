21c6e34c65945d32a7fc806a08f45cc9dba4d2df
Spencer Cloughley <spencercloughley@gmail.com>
2023-04-02 14:57:16 -0400

    Added verification for Player firstname, lastname and username. Slight changes to team-view to fit what is needed for assignment

--- src/main/java/com/example/assignment2/Player.java
+++ src/main/java/com/example/assignment2/Player.java
@@ -29,4 +29,133 @@ public class Player {
 
     //Characters they play in the game
     private ArrayList<String> champions;
+
+    public Player(String firstName, String lastName, String username, String position, int kills, int deaths, int assists, ArrayList<String> champions) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setUsername(username);
+        setPosition(position);
+        setKills(kills);
+        setDeaths(deaths);
+        setAssists(assists);
+        setKda();
+        setChampions(champions);
+    }
+
+    /**
+     * @return returns the first name of the player
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+    /**
+     *  set the first name of the player
+     *  validates that it is longer than 2 characters
+     *  sets it to format of "Spencer"
+     * @param firstName
+     */
+    public void setFirstName(String firstName) {
+        if(firstName.length()>2) {
+            firstName = firstName.toLowerCase();
+            firstName = firstName.substring(0,1).toUpperCase() +firstName.substring(1);
+            firstName = firstName.trim();
+            this.firstName = firstName;
+        }
+        else
+            throw new IllegalArgumentException(firstName + " invalid, must be longer than 2 characters");
+    }
+
+    /**
+     * @return returns the last name of the player
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     *  set the last name of the player
+     *  validates that it is longer than 2 characters
+     *  set it to format of "Cloughley"
+     * @param lastName
+     */
+    public void setLastName(String lastName) {
+        if(lastName.length()>2) {
+            lastName = lastName.toLowerCase();
+            lastName = lastName.substring(0,1).toUpperCase() + lastName.substring(1);
+            lastName = lastName.trim();
+            this.lastName = lastName;
+        }
+        else
+            throw new IllegalArgumentException(lastName + " invalid, must be longer than 2 characters");
+    }
+
+    /**
+     * @return returns the players username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     *  sets the username of the player
+     *  can be in any format, all uppercase, all lowercase, a mix, etc.
+     *  must be longer than 2 characters
+     *
+     * @param username
+     */
+    public void setUsername(String username) {
+        if(lastName.length()>2) {
+            this.lastName = lastName;
+        }
+        else
+            throw new IllegalArgumentException(lastName + " invalid, must be longer than 2 characters");
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getKills() {
+        return kills;
+    }
+
+    public void setKills(int kills) {
+        this.kills = kills;
+    }
+
+    public int getDeaths() {
+        return deaths;
+    }
+
+    public void setDeaths(int deaths) {
+        this.deaths = deaths;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    public double getKda() {
+        return kda;
+    }
+
+    public void setKda() {
+
+    }
+
+    public ArrayList<String> getChampions() {
+        return champions;
+    }
+
+    public void setChampions(ArrayList<String> champions) {
+        this.champions = champions;
+    }
 }
